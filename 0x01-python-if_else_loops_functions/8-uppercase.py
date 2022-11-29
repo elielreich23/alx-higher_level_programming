49ad514bf22deb05cb46f1928d89f928d0db2208
elielreich23 <elielreich@yahoo.com>
2022-11-29 20:11:46 -0300

    changes

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-    """Print a string in lowercase."""
-    if ord(str) >= 65 and ord(str) <= 90:
-        return True
-    else:
-        return False
+    """Print a string in upperrcase."""
+    for c in str:
+        if ord(c) >= 65 and ord(c) <= 90:
+            print("{}".format(c), end="")
