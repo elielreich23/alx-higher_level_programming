d591d2f460dcd11d69eafd4e64aa0427f92670a8
ELIEL DOUNA <97183542+elielreich23@users.noreply.github.com>
2022-11-29 21:39:43 -0300

    Update 100-print_tebahpla.py
    update

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0
+nc = 0
+for n in range(122, 96, -1):
+    if n % 2 == 0:
+        nc = n
+    else:
+        nc = n - 32
+    print('{}'.format(chr(nc)), end='')
