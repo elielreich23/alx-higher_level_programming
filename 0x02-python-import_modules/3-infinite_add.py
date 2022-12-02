334f36b4852b6d5afde9e43da3cd871b4ba436ea
elielreich23 <elielreich@yahoo.com>
2022-12-01 21:44:59 -0300

    modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import sys
     result = 0
     for arg in sys.argv:
-        if arg != sys.argv:
+        if arg != sys.argv[0]:
             result += int(arg)
     print(result)
