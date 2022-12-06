6435534eda8909ff485781e9bc8f7f7ec26fc5ef
elielreich23 <elielreich@yahoo.com>
2022-12-05 22:37:10 -0300

    reverse

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,6 @@
-#!/usr/bin/python3
-def element_at(my_list, element, idx):
-    if idx < 0:
-        return None
-    elif idx >= len(my_list):
-        return None
-    else:
-        my_list[idx] = element
+def replace_in_list(my_list, idx, element):
+    """Replace an element of a list at a specific position."""
+    if idx < 0 or idx > (len(my_list) - 1):
         return my_list
+    my_list[idx] = element
+    return my_list
