96c85b4cb3e074baab018c2f3538a378832c7315
elielreich23 <elielreich@yahoo.com>
2022-12-05 22:55:51 -0300

    reverse

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
     new_list = my_string
     string = ''
     for char in new_list:
-        if char !='c' and char != 'C':
+        if char != 'c' and char != 'C':
             string += char
     return string
