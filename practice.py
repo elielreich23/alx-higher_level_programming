26115e9b7334c206ba3e62354297f5ed53434f39
elielreich23 <elielreich@yahoo.com>
2022-12-06 21:51:10 -0300

    reverse

---
+++ practice.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+# basic aritmetics
+"""num1, num2 = input('enter two digits: ').split()
+num1 = int(num1)
+ num2 = int(num2)
+
+sum = num1 + num2
+diff = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+print("{} + {} = {}".format(num1, num2, sum))
+ print("{} - {} = {}".format(num1, num2, diff))
+print("{} * {} = {}".format(num1, num2, mul))
+print("{} / {} = {}".format(num1, num2, div))"""
+
+# recive miles and convert to km
+"""miles = input('Enter miles: ')
+miles = int(miles)
+kilometer = miles * 1.60934
+print("{} miles is equals to  kilometer".format(miles, kilometer))"""
+
+#calculator
+""" num1, num2 = input('Enter two digit: ').split()
+operator = input('entre operator: ')
+num1 = int(num1)
+num2 = int(num2)
+if operator == '+':
+    print("{} + {} = {}".format(num1, num2, num1 + num2))
+elif operator == '-':
+    print("{} - {} = {}".format(num1, num2, num1 - num2))
+elif operator == '*':
+    print("{} * {} = {}".format(num1, num2, num1 * num2))
+else:
+    print("{} / {} = {}".format(num1, num2, num1 / num2)) """
+
+
+# loop 
+for i in range(0, 101):
+    print(i)
+    # if (i % 2 == 0):
+    #     a = i
+
