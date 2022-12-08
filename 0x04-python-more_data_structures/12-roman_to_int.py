8d6e362b6607bcdb6b1d27a197b67cb9f309e513
elielreich23 <elielreich@yahoo.com>
2022-12-07 22:36:06 -0300

    dictionary

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_num = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     if (roman_string is None) or (type(roman_string) is not str):
         return 0
 
     num = len(roman_string)
-    value = roman_num[roman_string[num-1]]
+    value = roman[roman_string[num-1]]
     for i in range(num - 1, 0, -1):
-        current = roman_num[roman_string[i]]
-        previous = roman_num[roman_string[i-1]]
+        current = roman[roman_string[i]]
+        previous = roman[roman_string[i-1]]
 
         if previous >= current:
             value += previous
