b0cd326f52b31e26a64225276350d7e4840abc0d
elielreich23 <elielreich@yahoo.com>
2022-12-07 21:16:04 -0300

    matrix

--- 0x04-python-more_data_structures/1--search_replace.py
+++ 0x04-python-more_data_structures/1--search_replace.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list is not None:
-        list = []
-        for i, j in enumerate(my_list):
-            if my_list[i] == search:
-                list.append(replace)
-            else:
-                list.append(my_list[i])
-            return list
+    if not my_list:
+        return my_list
+    return [val if val != search else replace for val in my_list]
