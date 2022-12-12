1a5a422602ec308b9a457a5400af734d95cb5d3e
elielreich23 <elielreich@yahoo.com>
2022-12-12 14:52:32 -0300

    changes

--- practice.py
+++ practice.py
@@ -3,7 +3,6 @@
 """num1, num2 = input('enter two digits: ').split()
 num1 = int(num1)
  num2 = int(num2)
-
 sum = num1 + num2
 diff = num1 - num2
 mul = num1 * num2
@@ -35,8 +34,38 @@ else:
     print("{} / {} = {}".format(num1, num2, num1 / num2)) """
 
 
-for i in range(1, 101):
+"""for i in range(1, 101):
     if i%2 != 0:
         print(i, end=' ')
     elif i % 2 ==0:
-        print('even', end=' ')
+        print('even', end=' ')"""
+
+
+
+#print a pine three with 
+height = input("How tall is the tree:")
+
+height = int(height)
+
+space = height - 1
+
+hashes = 1
+
+stump_spaces = height - 1
+
+while height != 0:
+    for i in range(space):
+        print(' ', end="")
+    for i in range(hashes):
+        print('#', end="")
+        
+    print()
+
+    space -= 1
+    hashes +=2
+    height -=1
+
+for i in range(stump_spaces):
+    print(' ', end="")
+
+print("#")
