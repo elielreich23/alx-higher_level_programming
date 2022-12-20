ec637b8db87e4bdd72708462b012a3c8975f0007
elielreich23 <elielreich@yahoo.com>
2022-12-19 23:03:29 -0300

    exceptions

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_division(a,b):
+    q = 0
+    try:
+        q= a/b
+    except ZeroDivisionError:
+        q = None
+    except TypeError:
+        q = None
+    finally:
+        print("Inside result: {}".format(q))
+    return q
+
