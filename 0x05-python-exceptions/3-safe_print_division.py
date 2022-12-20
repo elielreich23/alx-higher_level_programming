228119d8db9d26da5b4e86c42296dbe19df78bea
elielreich23 <elielreich@yahoo.com>
2022-12-19 23:09:44 -0300

    exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def safe_print_division(a,b):
+def safe_print_division(a, b):
     q = 0
     try:
         q = a/b
@@ -10,4 +10,3 @@ def safe_print_division(a,b):
     finally:
         print('Inside result: {}'.format(q))
     return q
-    
