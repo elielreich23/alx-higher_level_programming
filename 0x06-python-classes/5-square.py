20be28ca1f1355e92a99776a42a32be7c5c40ecd
elielreich23 <elielreich@yahoo.com>
2022-12-21 11:16:23 -0300

    classes correction

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -5,38 +5,14 @@ This module defines a simple `Square` class
 
 
 class Square:
-    """A simple ``Square`` class
-    Attributes:
-        size (`int`): The size of the ``Square``.
-    """
     def __init__(self, size=0):
-        """Constructs a ``Square`` objet
-        Args:
-            size (`int`): The size of the ``Square``.
-                The default value is 0.
-        Raises:
-            TypeError: If ``size`` is not an integer.
-            ValueError: If ``size`` < 0
-        """
         self.size = size
 
     def area(self):
-        """Computes the area of the ``Square``.
-        Returns:
-            int: The area of the ``Square``.
-        """
         return self._Square__size ** 2
 
     @property
     def size(self):
-        """
-        Args:
-            size (`int`): The size of the ``Square``.
-                The default value is 0.
-        Raises:
-            TypeError: If ``size`` is not an integer.
-            ValueError: If ``size`` < 0
-        """
         return self._Square__size
 
     @size.setter
