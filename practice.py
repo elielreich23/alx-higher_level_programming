103a19b20f457f114aed7788891a535a7e16eb15
elielreich23 <elielreich@yahoo.com>
2022-12-23 12:45:29 -0300

    pratice

--- practice.py
+++ practice.py
@@ -41,8 +41,7 @@ else:
         print('even', end=' ')"""
 
 
-
-#print a pine three with 
+"""print a pine three with 
 height = input("How tall is the tree:")
 
 height = int(height)
@@ -68,4 +67,72 @@ while height != 0:
 for i in range(stump_spaces):
     print(' ', end="")
 
-print("*")
+print("*")"""
+"""
+#return a list of prime
+#input maximum prime
+#use for to loop through
+#function to check for reminder
+def prime_num(num):
+    for i in range(2, num):
+        if (num % i) == 0:
+            return False
+    
+    return True
+
+#function to get prime_num list
+def get_prime__num(max_num):
+    prime_list = []
+    for number in range(2, max_num):
+        if prime_num(number):
+            prime_list.append(number)
+    return prime_list
+
+#ask user to input max number to check
+max_num_check = int(input("Enter the max number to check:"))
+prime_list = get_prime__num(max_num_check)
+for primes in prime_list:
+    print(primes)"""
+
+"""
+to check for unkown number of argumets we use 
+(*args) args 
+"""
+"""
+#calculator that calculate area of differents shapes
+
+#get_are&a func
+import math
+def get_area(shape):
+    shape = shape.lower()
+    #more shape can be created with more conditional statements and the function to calculate the shape
+    if shape == "rectangle":
+        rect_area()
+    elif shape == "circle":
+        circ_area()
+    else:
+        print("Enter a shape")
+
+#func to calcuate rectangle area
+def rect_area():
+    lenght = float(input("Enter the lenght:"))
+    width = float(input("Enter the width:"))
+
+    area = lenght * width
+
+    print("The area of the rectangle is: ", area)
+
+def circ_area():
+    radius = float(input("Enter the radius:"))
+    
+    area = math.pi * (math.pow(radius, 2))
+
+    print("The area of the circle is {:.2f}: ".format(area)) 
+
+#ask user for shape
+def main():
+    shape_type = input("Enter a shape:")
+
+    #function to calculate area
+    get_area(shape_type)
+main()"""
