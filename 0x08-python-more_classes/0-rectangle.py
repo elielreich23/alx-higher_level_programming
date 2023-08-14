fef9f99374ec622c23ec16fa782eb1821fb33727
ELIEL DOUNA <97183542+elielreich23@users.noreply.github.com>
2023-08-14 20:39:04 -0300

    updated files
    update

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""Class to define Rectangle"""
+
+class Rectaangle:
+    """props of the rectangle"""
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""Class to define Rectangle"""
+class Rectangle:
+    """porps of the rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """initialize the rectangle
+        Args:
+            width: the width of the rectangle
+            height: the height
+        """
+        self.width = width
+        self.height = height
+    
+    @property
+    def width(self):
+        return self.width
+    
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("the width must be an integer")
+        if value < 0:
+            raise TypeError("width must be >=0")
+        self.width = value
+    
+    @property
+    def height(self):
+        return self.height
+    
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("the height must be an integer")
+        if value < 0:
+            raise TypeError("height must be >=0")
+        self.height = value
